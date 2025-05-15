779723c9296fa6e17798fda3ddd4a5b3292610aa
Michael Skiba <michael@skiba.dev>
2025-05-15 13:32:00 +0200

    Pulled in Scroll view excercise

---
+++ 4.1 - Animations Excercises/Animations - Finished/Animations/App/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Layouts
+//
+//  Created by Michael Skiba on 13/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RootView()
+        }
+    }
+}
